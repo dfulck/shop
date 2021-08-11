7ea29e5b8cd983fba1ad0dafee08ae2fa4034ab4
dfulck <dfulk@student.cccd.edu>
2021-08-11 11:41:01 +0430

    web templates added to the project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+Route::get('/panel',function (){
+    return view('Admin.Panel');
 });
